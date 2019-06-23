3970ff9ed0d5255338df78b7d8ed486f560d72e6
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-06-23 14:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code here!
 main.remove();
 
-var newHeader = document.querySelector("h1#victory");
+let newHeader = document.querySelector("h1#victory");
 newHeader.innerHTML = "YOUR-NAME is the champion"
