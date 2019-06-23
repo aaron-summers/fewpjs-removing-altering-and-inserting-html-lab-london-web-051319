b8b8915e72448923a6b43c16c83c34fb7c0afe83
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-06-23 14:53:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var element = document.createElement("h1");
 document.body.appendChild(element)
 var headerChild = document.body.element.appendChild("victory")
 
-// var newHeader = document.querySelector("h1#victory");
+var newHeader = document.querySelector("h1#victory");
 // newHeader.innerHTML = "YOUR-NAME is the champion"
