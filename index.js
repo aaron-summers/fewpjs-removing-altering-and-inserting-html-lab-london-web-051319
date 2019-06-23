81267ba915668320e239e50f93a9738c7195fc53
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-06-23 14:55:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Write your code here!
 main.remove();
 
-var element = document.createElement("h1");
-document.body.appendChild(element)
-var headerChild = document.body.element.appendChild("victory")
+let newHeader = document.createElement("h1");
 
-var newHeader = document.querySelector("h1#victory");
-// newHeader.innerHTML = "YOUR-NAME is the champion"
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "YOUR-NAME is the champion!";
